18f232fa85bc2db04289844a053939e61eaa6ad1
まつ <92625267+matsukz@users.noreply.github.com>
2023-01-17 22:14:26 +0900

    Update ClassNotice.py

--- ClassNotice.py
+++ ClassNotice.py
@@ -1,12 +1,17 @@
 import json
 import datetime
 import schedule
+import time
 
-dt = datetime.date.today()
+print("===START===")
+print(datetime.datetime.now())
+print("\n")
+
+NowDate = datetime.date.today()
 
 impjson = json.load(
     open(
-        "class.json",
+        "classcopy.json",
         "r",
         encoding="utf-8"
     )
@@ -22,13 +27,45 @@ Week = [
 ]
 
 i = 0
-while not list(impjson.keys())[i] == Week[dt.weekday()]:
+while not list(impjson.keys())[i] == Week[NowDate.weekday()]:
     i = i + 1
 Today = list(impjson.keys())[i]
 
 i = 1
-print("===START===")
-while not impjson[Today][str(i)]["Class"] == "END":
-    print(impjson[Today][str(i)]["Class"])
-    i = i + 1
-print("===END===")
+
+def CheckTime():
+    global i
+    print("===== " + str(i) + " =====")
+
+    Now = datetime.datetime.now()
+    NowTime = str(Now.hour) + str(Now.minute)
+    print("NowTime = " + NowTime)
+
+    try:
+        print("JSON DATE Time = " + impjson[Today][str(i)]["Time"])
+
+        if impjson[Today][str(i)]["Time"] == "END":
+            print("END")
+            print("====END====")
+            exit()
+
+        elif impjson[Today][str(i)]["Time"] == NowTime:
+            print(impjson[Today][str(i)]["Class"])
+            i = i + 1
+            print("===Next " + str(i) + " ===")
+
+        else:
+            print("NOT MATCH")
+
+    except TypeError:
+        print("SKIP NULL")
+        i = i + 1
+        print("==============")
+    
+    print("\n")
+
+schedule.every(1).minutes.do(CheckTime)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
